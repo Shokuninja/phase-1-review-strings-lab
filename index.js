29cccce61c531aaaf08edf794d1a1fab16dd1975
Lohan <ltwm1998@gmail.com>
2023-09-29 19:00:27 -0400

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+
+const currentUser = 'Dude';
+
+// console.log(`${currentUser}`)
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// console.log(`${welcomeMessage}`)
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// console.log(`${excitedWelcomeMessage}`)
+
+const firstInitial = currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+console.log(`${shortGreeting}`)
